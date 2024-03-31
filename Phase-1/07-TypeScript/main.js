98b19b776d531c1abb38aa942eb65e87675cfd53
Good-Llama <159387211+Good-Llama@users.noreply.github.com>
2024-03-30 21:02:21 -0400

    Delete Phase-1 directory

--- Phase-1/07-TypeScript/main.js
+++
@@ -1,5 +0,0 @@
-// TypeScript starter code for class
-
-console.log(aName)
-//  let aName - "Moe Howard"    //let variable must be defined before use
-var aName - "Moe Howard"        //let variable must be defined before use
